0dbaf1b76829354381c4cdc4bbd27e5f09eaac62
Codemonke123 <kadunayan86@gmail.com>
2026-08-13 13:41:15 +0530

    Few changes as actually adding the dataset for quick access and usage

--- Exercise_Python_Programming/Arithmatic_and_Variable/Problem_1.py
+++ Exercise_Python_Programming/Arithmatic_and_Variable/Problem_1.py
@@ -9,8 +9,3 @@ secs_per_min = 60
 total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
 print(total_secs)
 
-#In general, to work with a variable, you need to begin by selecting the name you want to use. Variable names are ideally short and descriptive. They also need to satisfy several requirements:
-
-# 1.They can't have spaces (e.g., test var is not allowed)
-# 2.They can only include letters, numbers, and underscores (e.g., test_var! is not allowed)
-# 3.They have to start with a letter or underscore (e.g., 1_var is not allowed)
--- Exercise_Python_Programming/Hello.py
+++ Exercise_Python_Programming/Hello.py
@@ -1,2 +1,8 @@
 print("Hello, World!")
 # Setup done for Python Programming Exercise.
+
+#In general, to work with a variable, you need to begin by selecting the name you want to use. Variable names are ideally short and descriptive. They also need to satisfy several requirements:
+
+# 1.They can't have spaces (e.g., test var is not allowed)
+# 2.They can only include letters, numbers, and underscores (e.g., test_var! is not allowed)
+# 3.They have to start with a letter or underscore (e.g., 1_var is not allowed)
